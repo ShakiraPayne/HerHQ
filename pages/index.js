7977fff7df80785ace7c22f3b0b6fe1115f740bf
Vishal Sharma <massifcoder>
2024-04-04 20:51:50 +0530

    add context menu handler

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home(){
     <div className={`portslat h-screen relative pb-16 bg-white`}>
       <Contact/>
       <Header/>
-      <div className="flex items-center justify-center min-h-2/3">
-        <video className="md:hidden block" autoPlay loop muted preload="auto" controls={false}>
+      <div className="flex items-center justify-center min-h-2/3 video-container">
+        <video className="md:hidden block" onContextMenu={e => e.preventDefault()} autoPlay={true} loop muted={true} preload="auto" controls={false}>
           <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391165/herhq/video/p7nxxpiwas4thslg1mju_1.mp4" type="video/mp4" />
         </video>
-        <video className="hidden md:block w-2/3" autoPlay loop muted preload="auto" controls={false}>
+        <video className="hidden md:block w-2/3" onContextMenu={e => e.preventDefault()} autoPlay={true} loop muted={true} preload="auto" controls={false}>
           <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391162/herhq/video/vzswpwjeoutvhl66ntvj_2.mp4" type="video/mp4" />
         </video>
       </div>
