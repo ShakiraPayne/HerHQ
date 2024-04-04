d366aeaff929c6e3431ce7a0c08370beb572550a
Vishal Sharma <massifcoder>
2024-04-04 20:36:33 +0530

    disabled controls

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home(){
       <Contact/>
       <Header/>
       <div className="flex items-center justify-center min-h-2/3">
-        <video className="md:hidden block" autoPlay loop muted preload="auto">
+        <video className="md:hidden block" autoPlay loop muted preload="auto" controls={false}>
           <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391165/herhq/video/p7nxxpiwas4thslg1mju_1.mp4" type="video/mp4" />
         </video>
-        <video className="hidden md:block w-2/3" autoPlay loop muted preload="auto">
+        <video className="hidden md:block w-2/3" autoPlay loop muted preload="auto" controls={false}>
           <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391162/herhq/video/vzswpwjeoutvhl66ntvj_2.mp4" type="video/mp4" />
         </video>
       </div>
