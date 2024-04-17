e769c61a17d01ab55ffb668416f9dc8bca084363
massifcoder <vishalsharma243527@gmail.com>
2024-04-18 01:50:57 +0530

    done

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#FF0075" />
+        <meta name="description" content="Clothing Brand" />
+        <meta name="keywords" content="Clothing, Brand, Fashion, Style" />
+        <meta name="author" content="Clothing Brand" />
+        <link rel="apple-touch-icon" href="/icons/logo.png" />
+        <link rel="icon" href="/icons/favicon.jpg" className="bg-white"/>
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,56 @@
-import Header from "./components/common/header";
+import Image from "next/image";
 import Contact from "./components/common/contact";
+import Header from "./components/common/header";
+import Varities from './components/home/varities'
 import Footer from "./components/common/footer";
-import Varities from "./components/home/varities";
-import Ambassador from "./components/home/ambassador";
+import Ambassador from './components/home/ambassador'
 
-export default function Home(){
+export default function Home() {
+
   return (
     <div className={`portslat h-screen relative pb-16 bg-white`}>
-      <Contact/>
-      <Header/>
+      <Contact />
+      <Header />
       <div className="flex items-center md:pb-12 justify-center min-h-2/3 video-container">
-        <video className="md:hidden block" onContextMenu={e => e.preventDefault()} autoPlay={true} loop muted={true} preload="auto" playsInline controls={false}>
-          <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391165/herhq/video/p7nxxpiwas4thslg1mju_1.mp4" type="video/mp4" />
-        </video>
-        <video className="hidden md:block w-2/3" onContextMenu={e => e.preventDefault()} autoPlay={true} loop muted={true} playsInline preload="auto" controls={false}>
-          <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391162/herhq/video/vzswpwjeoutvhl66ntvj_2.mp4" type="video/mp4" />
-        </video>
+        <div className="relative">
+          <Image src='/small.png' className="block absolute top-0 md:hidden z-10" width={700} height={500} alt="Placeholder" />
+          <div className="relative z-20">
+          <video
+            className='block top-0 md:hidden z-50'
+            onContextMenu={e => e.preventDefault()}
+            autoPlay
+            loop
+            muted
+            preload='auto'
+            playsInline
+            controls={false}
+          >
+            <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391165/herhq/video/p7nxxpiwas4thslg1mju_1.mp4" type="video/mp4" />
+          </video>
+          </div>
+        </div>
+        <div className="relative">
+          <Image src='/full.png' className="hidden w-80 absolute md:block z-10 " width={900} height={800} alt="Placeholder" />
+          <div className="relative top-0 z-20">
+          <video
+            className='hidden md:block z-20'
+            onContextMenu={e => e.preventDefault()}
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload='auto'
+            controls={false}
+          >
+            <source src="https://res.cloudinary.com/dcpqx91e5/video/upload/v1711391162/herhq/video/vzswpwjeoutvhl66ntvj_2.mp4" type='video/mp4' />
+          </video>
+          </div>
+        </div>
       </div>
-      <Varities/>
-      <Ambassador/>
-      <Footer/>
+      <Varities />
+      <Ambassador />
+      <Footer />
       <div className="h-16 xs:h-0"></div>
     </div>
-  )
+  );
 }
