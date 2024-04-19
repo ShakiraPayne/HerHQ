04e3f06b03010a1b75605b609d638fc1320d2e35
massifcoder <vishalsharma243527@gmail.com>
2024-04-19 23:32:17 +0530

    issue resolved #6718

--- pages/installGuide/index.js
+++ pages/installGuide/index.js
@@ -1,33 +1,35 @@
-import Contact from "../components/common/contact"
-import Footer from "../components/common/footer"
-import Header from "../components/common/header"
+import Contact from "../components/common/contact";
+import Footer from "../components/common/footer";
+import Header from "../components/common/header";
 
 export default function InstallGuide() {
-    return <div>
-        <Contact />
-        <Header />
-        <div>
-            <h1 className="font-bold text-2xl">Installation Steps:</h1>
-            <div className="mt-4">
-                <h1 className="text-xl font-semibold">In Safari</h1>
-                <ol>
-                    <li>Click on the Share button in the Safari browser.</li>
-                    <li>Click on the Add to Home Screen button.</li>
-                    <li>Click on the Add button.</li>
-                    <li>On home screen, you can see the app named HerHq.</li>
-                </ol>
+    return (
+        <div className="p-4 md:p-8">
+            <Contact />
+            <Header />
+            <div className="max-w-3xl md:pt-8 pt-6 mx-auto text-center">
+                <h1 className="font-bold text-2xl mb-6">Installation Steps:</h1>
+                <div className="mt-4">
+                    <h1 className="text-xl font-semibold">In Safari</h1>
+                    <ol className="list-decimal pl-4">
+                        <li>Click on the Share button in the Safari browser.</li>
+                        <li>Click on the Add to Home Screen button.</li>
+                        <li>Click on the Add button.</li>
+                        <li>On the home screen, you can see the app named HerHq.</li>
+                    </ol>
+                </div>
+                <div className="mt-8">
+                    <h1 className="text-xl font-semibold">In Chrome</h1>
+                    <ol className="list-decimal pl-4">
+                        <li>Click on the three dots in the Chrome browser.</li>
+                        <li>Click on the Add to Home Screen button.</li>
+                        <li>Click on the Add button.</li>
+                        <li>On the home screen, you can see the app named HerHq.</li>
+                    </ol>
+                </div>
             </div>
-            <div className="mt-4">
-                <h1 className="text-xl font-semibold">In Chrome</h1>
-                <ol>
-                    <li>Click on the three dots in the Chrome browser.</li>
-                    <li>Click on the Add to Home Screen button.</li>
-                    <li>Click on the Add button.</li>
-                    <li>On home screen, you can see the app named HerHq.</li>
-                </ol>
-            </div>
+            <Footer />
+            <div className="md:h-0 h-16"></div>
         </div>
-        <Footer />
-        <div className="md:h-0 h-16"></div>
-    </div>
+    );
 }
