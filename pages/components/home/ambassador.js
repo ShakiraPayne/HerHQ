3f1293c990cc9c91e95a19889d6775639e581b3f
massifcoder <vishalsharma243527@gmail.com>
2024-04-22 07:50:24 +0530

    card

--- pages/components/home/ambassador.js
+++ pages/components/home/ambassador.js
@@ -10,7 +10,7 @@ export default function Ambassador() {
                 <div className="text-3xl my-6 font-bold text-center">@herhqofficial</div>
                 <div className="p-4 text-center max-w-[500px] text-gray-600">See the styles 💗 by others and share the styles you 💗</div>
             </div>
-            <div className="flex overflow-x-scroll snap-x md:flex-wrap bg-white content-center md:p-6 gap-6 place-items-center place-content-center w-screen">
+            <div className="flex overflow-x-scroll snap-x md:flex-wrap bg-white content-center md:p-6 gap-6 place-items-center place-content-center w-screen flex-shrink-0">
                 <Card img={"/ambassador/amb1.jpg"} />
                 <Card img={"/ambassador/amb2.jpg"} />
                 <Card img={"/ambassador/amb3.jpg"} />
